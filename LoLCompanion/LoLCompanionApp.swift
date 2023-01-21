6376123d29522d4195445d3cb61336b97c714c58
Jean-Louis Darmon <luis@beamapp.co>
2023-01-21 17:36:47 +0100

    Initial Commit

---
+++ LoLCompanion/LoLCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoLCompanionApp.swift
+//  LoLCompanion
+//
+//  Created by Jean-Louis Darmon on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoLCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
